e5ba3d1e9f084a0b1ff3e9e3fbee073fa3dcf971
Korosuro <140105526+Korosuro@users.noreply.github.com>
2024-03-24 18:26:57 +0100

    Update frontend.py

--- PyEx/Contact List/frontend.py
+++ PyEx/Contact List/frontend.py
@@ -1,9 +1,34 @@
 from tkinter import *
+from tkinter import ttk
+import tkinter as tk
 
 window = Tk()
-window.title("Kontaktliste")        #adding a title
-window.config(bg="blue")            #changing backgroundd color
-window.minsize(200, 200)            #setting a fix minimum-size     without setting max-size, its unlimited
-window.geometry("300x300+50+50")    #width x height + x + y
+window.title("Kontaktliste")            #adding a title
+window.config(bg="blue")                #changing backgroundd color
+window.minsize(200, 200)                #setting a fix minimum-size     without setting max-size, its unlimited
+window.geometry("700x500")
+
+#creating a label, which can be used to display either text or images
+text = Label(window, text="Test-Text")
+text.pack()
+text2 = Label(window, text="--Wisdom")
+text2.pack()
+
+exit = tk.Button(window, text="Exit the app", command = window.destroy)
+exit.pack()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 window.mainloop()
 
