3bdbc8eda315943b9dca008723d2e513b523f196
Korosuro <140105526+Korosuro@users.noreply.github.com>
2024-03-25 23:39:32 +0100

    Frontend Input Finish

--- PyEx/Contact List/backend.py
+++ PyEx/Contact List/backend.py
@@ -1,20 +1,24 @@
 import mysql.connector
 conn = mysql.connector.connect(host="localhost", password="+C,+E}>{kk}$dO36", user="root", database="contactlist")
 
+
 mycursor =  conn.cursor()
-vorname = str(input("Vorname: "))
-nachname = str(input("Nachname: "))
-telnum = int(input("Telefonnummer: "))
-mail = str(input("Mail-Adresse: "))
-adress = str(input("Wohnort: "))
+#vorname = str(input("Vorname: "))
+#nachname = str(input("Nachname: "))
+#telnum = int(input("Telefonnummer: "))
+#mail = str(input("Mail-Adresse: "))
+#adress = str(input("Wohnort: "))
 
-sql = "INSERT INTO contacts (Nachname, Vorname, Telefonnummer, Mail) VALUES (%s, %s, %s, %s)"
-data = (vorname, nachname, telnum, mail)
+def data_input(vorname, nachname, telnum, mail, adress):
+    sql = "INSERT INTO contacts (Nachname, Vorname, Telefonnummer, Mail, Wohnort) VALUES (%s, %s, %s, %s, %s)"
+    data = (nachname, vorname, telnum, mail, adress)
 
-mycursor.execute(sql, data)
-conn.commit()
+    mycursor.execute(sql, data)
+    conn.commit()
 
-mycursor.close()
-conn.close()
+    mycursor.close()
+    conn.close()
 
-print("Success")
+    print("Success")
+
+    
--- PyEx/Contact List/frontend.py
+++ PyEx/Contact List/frontend.py
@@ -2,6 +2,7 @@ from tkinter import *
 import tkinter as tk
 import mysql.connector
 conn = mysql.connector.connect(host="localhost", password="+C,+E}>{kk}$dO36", user="root")
+import backend
 
 root = Tk()
 root.title("Kontaktliste")            
@@ -9,13 +10,44 @@ root.title("Kontaktliste")
 root.minsize(200, 200)             
 root.geometry("700x500")
 
-exit = tk.Button(root, text="Exit the app", command = root.destroy).grid(row=1, column=1)
+exit = tk.Button(root, text="Exit the app", command = root.destroy).grid(row=0, column=0)
 #exit.pack()
 
+vornamepre = Label(root, text="Vorname: ")
+vornamepre.grid(column=0, row=1)
+vorname = Entry(root)
+vorname.grid(column=1, row=1)
 
+nachnamepre = Label(root, text="Nachname bitte: ")
+nachnamepre.grid(column=0, row=2)
+nachname = Entry(root)
+nachname.grid(column=1, row=2)
 
+telnumpre = Label(root, text="Telefonnummer bitte: ")
+telnumpre.grid(column=0, row=3)
+telnum = Entry(root)
+telnum.grid(column=1, row=3)
 
+mailpre = Label(root, text="E-Mail-Adresse bitte: ")
+mailpre.grid(column=0, row=4)
+mail = Entry(root)
+mail.grid(column=1, row=4)
 
+adresspre = Label(root, text="Wohnort bitte: ")
+adresspre.grid(column=0, row=5)
+adress = Entry(root)
+adress.grid(column=1, row=5)
 
+def submit_data():
+    vorname_value = vorname.get()
+    nachname_value = nachname.get()
+    telnum_value = telnum.get()
+    mail_value = mail.get()
+    adress_value = adress.get()
+
+    backend.data_input(vorname_value, nachname_value, telnum_value, mail_value, adress_value)
+
+submit_button = Button(root, text="Bestätigen", command=submit_data)     
+submit_button.grid(column=0, row=6)
 
 root.mainloop()
